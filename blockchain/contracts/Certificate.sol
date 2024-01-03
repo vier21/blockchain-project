4dc2d51b759208b21833cd7347351f7f262d542e
vier21 <pilyaixavier21@gmail.com>
2024-01-03 08:45:47 +0700

    cert

--- blockchain/contracts/Certificate.sol
+++ blockchain/contracts/Certificate.sol
@@ -1,41 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract CertificateStorage {
+contract VaccineCertificateStorage {
     
-    struct Certificate {
+    struct VaccineCertificate {
         uint256 id;
         address issuer;
         address owner;
-        string name;
-        string issuerOrganization;
-        uint256 date; 
+        string vaccineType;
+        uint256 dateOfVaccination;
+        uint256 dosage;
         bool isValid;
     }
 
-    Certificate[] public certificates;
+    VaccineCertificate[] public vaccineCertificates;
     mapping(address => uint256[]) public ownerCertificates;
 
     event CertificateCreated(uint256 indexed id, address indexed issuer, address indexed owner);
 
-    function getCertificates() public view returns (Certificate[] memory) {
-        return certificates;
+    function getCertificates() public view returns (VaccineCertificate[] memory) {
+        return vaccineCertificates;
     }
 
-    function issueCertificate(address _to, string memory _name, string memory _issuerOrganization) public returns (uint256) {
-        uint256 newId = certificates.length;
+    function issueCertificate(address _to, string memory _vaccineType, uint256 _dateOfVaccination, uint256 _dosage) public returns (uint256) {
+        uint256 newId = vaccineCertificates.length;
 
-        Certificate memory newCertificate = Certificate({
+        VaccineCertificate memory newCertificate = VaccineCertificate({
             id: newId,
             issuer: msg.sender,
             owner: _to,
-            name: _name,
-            issuerOrganization: _issuerOrganization,
-            date: block.timestamp,
+            vaccineType: _vaccineType,
+            dateOfVaccination: _dateOfVaccination,
+            dosage: _dosage,
             isValid: true
         });
 
-        certificates.push(newCertificate);
+        vaccineCertificates.push(newCertificate);
         ownerCertificates[_to].push(newId);
 
         emit CertificateCreated(newId, msg.sender, _to);
@@ -43,21 +43,21 @@ contract CertificateStorage {
         return newId;
     }
 
-    function validateCertificate(uint256 _id) public view returns (string memory) {
-        require(_id < certificates.length, "Invalid certificate ID");
-        
-        if (certificates[_id].isValid) {
-            return "Certificate is Valid";
-        } else {
-            return "Certificate is Invalid";
-        }
+    function getVaccineCertificate(uint256 _id) public view returns (VaccineCertificate memory) {
+        require(_id < vaccineCertificates.length, "Invalid certificate ID");
+        return vaccineCertificates[_id];
+    }
+
+    function validateCertificate(uint256 _id) public view returns (VaccineCertificate memory) {
+        require(_id < vaccineCertificates.length, "Invalid certificate ID");
+        return vaccineCertificates[_id];
     }
 
     function invalidateCertificate(uint256 _id) public {
-        require(_id < certificates.length, "Invalid certificate ID");
-        require(msg.sender == certificates[_id].issuer, "You are not the issuer of this certificate");
+        require(_id < vaccineCertificates.length, "Invalid certificate ID");
+        require(msg.sender == vaccineCertificates[_id].issuer, "You are not the issuer of this certificate");
 
-        certificates[_id].isValid = false;
+        vaccineCertificates[_id].isValid = false;
     }
 
     function getOwnerCertificates(address _owner) public view returns (uint256[] memory) {
